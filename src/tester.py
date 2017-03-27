e40aa254a0aaee8d96a6f6fc671101436ebd7909
Eduard Čuba <cuba.edo@gmail.com>
2017-03-27 11:50:37 +0200

    [tests]: introduce tests for mathlib
    - priority tests
    - parentheses
    - negative values

--- src/tester.py
+++ src/tester.py
@@ -22,5 +22,35 @@ class mlib_test(unittest.TestCase):
         self.assertEqual(self.solver.solve('10/13'), 10/13)
         self.assertEqual(self.solver.solve('8/0'), ZeroDivisionError)
 
+    def test_negative(self):
+        self.assertEqual(self.solver.solve('0 - 100'), -100)
+        self.assertEqual(self.solver.solve('-5 + 1'), -4)
+        self.assertEqual(self.solver.solve('-5 * 2'), -10)
+        self.assertEqual(self.solver.solve('-5 / 1'), -5)
+
+    def test_priority_multiplication(self):
+        task = '2 * 3 + 4'
+        self.assertEqual(self.solver.solve(task), 10)
+        task = '2 + 3 * 4'
+        self.assertEqual(self.solver.solve(task), 14)
+
+    def test_priority_division(self):
+        task = '4 / 2 - 2'
+        self.assertEqual(self.solver.solve(task), 0)
+        task = '4 - 2 / 2'
+        self.assertEqual(self.solver.solve(task), 3)
+
+    def test_priority_parentheses(self):
+        task = '4 / (2 - 1)'
+        self.assertEqual(self.solver.solve(task), 4)
+        task = '2 * (3 + 4)'
+        self.assertEqual(self.solver.solve(task), 14)
+
+    def test_negative_parentheses(self):
+        self.assertEqual(self.solver.solve('-5 + (-10)'), -15)
+        self.assertEqual(self.solver.solve('-10 - (-5)'), -5)
+        self.assertEqual(self.solver.solve('-10 * (-5)'), 50)
+        self.assertEqual(self.solver.solve('10 * (-5)'), -50)
+        self.assertEqual(self.solver.solve('-10 * (-0)'), 0)
 
 unittest.main(verbosity=2)
